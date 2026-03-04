e1ca7e2ac3f17f450b4f64f8012f9e7145dcc3d4
unknown <ankurkumar98456@gmail.com>
2026-03-04 15:03:20 +0530

    added

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: '/api',
+    baseURL: 'https://profound-fascination-production-0fb5.up.railway.app/api',
     headers: {
         'Content-Type': 'application/json',
     },
